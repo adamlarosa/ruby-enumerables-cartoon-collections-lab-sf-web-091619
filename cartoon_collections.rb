b5de0292ccfb26d9196d971190dabfd495986e73
Adam La Rosa <adam.larosa@gmail.com>
2019-09-09 20:54:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(array) # code an argument here
   # Your code here
-  array.each_with_index { |dwarf, index| 
-    puts "#{index + 1} #{dwarf[index]}" }
+  array.each_with_index { |dwarf, index| puts dwarf[index] }
   
 end
 
