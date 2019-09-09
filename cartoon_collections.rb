84b05e15393a5e36519e929f4c4ee59f4e86157a
Adam La Rosa <adam.larosa@gmail.com>
2019-09-09 21:56:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,8 @@ def long_planeteer_calls(array) # code an argument here
   # Your code here
   
   if array.each {|call| call.size > 4 } then return true
-else return false
-end
+  else return false
+  end
 end
 
 def find_the_cheese# code an argument here
