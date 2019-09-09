04c8e7ccbdc243bc9b212ebc0003de2afb9b3d91
Adam La Rosa <adam.larosa@gmail.com>
2019-09-09 21:24:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array) # code an argument here
   # Your code here
   
   if array.each_with_index {|call, index| 
-    call[index].length >= 5 } then return true
+    call[index].length > 4 } then return true
     else return false
   end
   
