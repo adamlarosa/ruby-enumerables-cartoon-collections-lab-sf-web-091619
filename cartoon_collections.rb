1e42f12261cfaeb3eb2558018f56257055479fb2
Adam La Rosa <adam.larosa@gmail.com>
2019-09-09 21:05:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(calls) # code an argument here
   # Your code here
   
   new=[]
-  calls.collect { |call| new.push(call.capitalize)}
+  calls.collect { |call| new.push(call.capitalize + "!")}
   
 end
 
