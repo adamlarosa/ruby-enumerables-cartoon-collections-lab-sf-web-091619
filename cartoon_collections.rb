cf625d0084b14783ab6ca709279238bd5b4b0be6
Adam La Rosa <adam.larosa@gmail.com>
2019-09-09 22:00:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,13 +13,13 @@ end
 
 def long_planeteer_calls(array) # code an argument here
   # Your code here
-  
-  if array.all? {|call| call.size <= 4 } then return false
-  else return true
+  if array.all? {|call| call.size <= 4 } 
+    then return false
+    else return true
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
