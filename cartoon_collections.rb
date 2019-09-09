e5c492110894a5130cf9f879af7d10520c8b2c60
Adam La Rosa <adam.larosa@gmail.com>
2019-09-09 22:41:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,13 @@ def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.any? { |cheese| if cheese_types.include?(cheese) then return cheese end }
+  array.any? { |cheese| 
+    if cheese_types.include?(cheese) 
+      then return cheese
+    else
+      return nil
+    end 
+  }
 
 
   
